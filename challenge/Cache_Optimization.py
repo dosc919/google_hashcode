f1a7594293ec06b0921593efb89c5729a053cdd0
michaeljantscher <michael.jantscher19@gmail.com>
2017-02-23 21:03:43 +0100

    classes ready

--- challenge/Cache_Optimization.py
+++ challenge/Cache_Optimization.py
@@ -35,41 +35,24 @@ cache_map = {}
 
 for it in range(int(init_line[0])):
     video_map[it] = Video(it, int(video_size_line[it]))
+last_tmp = start_endpoint_line[len(start_endpoint_line) - 1]
+first_video_request_indx = last_tmp + int(fileContent[last_tmp].split(' ')[1]) + 1
+
+for it in range(first_video_request_indx, len(fileContent)):
+    video_tmp, endpoint_tmp, nr_requests_tmp = fileContent[it].split(' ')
+    video_map[int(video_tmp)].addNrRequestToEnpoint(int(endpoint_tmp), int(nr_requests_tmp))
+    
 
 for it in range(int(init_line[1])):
     nr_lat = int(fileContent[start_endpoint_line[it]].split(' ')[1])
     endpoint_map[it] = Endpoint(it, int(fileContent[start_endpoint_line[it]].split(' ')[0]))
-    for it_lat in range((start_endpoint_line[it] + 1), (start_endpoint_line[it] + nr_lat)):
-        cache_id = fileContent[it_lat].split(' ')[1]
-        latency_tmp = fileContent[it_lat].split(' ')[0]
+    for it_lat in range((start_endpoint_line[it] + 1), (start_endpoint_line[it] + nr_lat + 1)):
+        cache_id = int(fileContent[it_lat].split(' ')[1])
+        latency_tmp = int(fileContent[it_lat].split(' ')[0])
         endpoint_map[it].addCacheEndpointLatency(cache_id, latency_tmp)
         
         
-
 for it in range(int(init_line[3])):
-    cache_map[it] = Cache(it, 0)
+    cache_map[it] = Cache(it, int(init_line[4]))
     
-    
-
-#nr_cols = int(fileContent[0])
-#nr_rows = int(fileContent[2])
-#
-#fileContent = fileContent.replace("\n", "")
-#pizzaContent = fileContent[7:]
-#pizzaContent = np.array(list(pizzaContent))
-#pizza_matrix = np.reshape(pizzaContent, (nr_cols, nr_rows))
-#
-#
-#print ('write file')
-#out_path = 'input/output.in'
-#fo = open(out_path, "w")
-#
-##for ind in range(0,np.size(pizza_matrix,0)):
-##    fo.write( str(pizza_matrix[ind,:])+ "\n");
-#  # Close file
-#fo.write(str(3) + "\n" + str(0) + " " + str(0) + " "+str(2) + " " +
-# str(1) + "\n" + str(0) + " " + str(2)+ " " + str(2)+ " " +
-# str(2) + "\n" + str(0) + " " +str(3) + " " +str(2) + " "+ str(4))
-#fo.close()
-
 print("done")
