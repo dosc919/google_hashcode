b640dae7b373227a70a8e5bdcbba51e860557cc0
Stefan Ainetter <stefan.ainetter@student.tugraz.at>
2017-02-23 16:23:29 +0100

    asdf

---
+++ practice/Pizza_ex.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 15:14:29 2017
+
+@author: root
+"""
+filname = '/input/small.in'
+with open(fileName, mode='rb') as file: # b is important -> binary
+    fileContent = file.read()
