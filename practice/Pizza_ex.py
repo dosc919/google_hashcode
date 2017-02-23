8447daa2d0d351c3682a524d89cea0c3441417c4
Stefan Ainetter <stefan.ainetter@student.tugraz.at>
2017-02-23 17:11:08 +0100

    write file

--- practice/Pizza_ex.py
+++ practice/Pizza_ex.py
@@ -5,9 +5,6 @@ Created on Thu Feb 23 15:14:29 2017
 @author: dom,jantschi,stef
 """
 
-import os
-import requests
-from bs4 import BeautifulSoup
 import numpy as np
 
 
@@ -24,5 +21,15 @@ fileContent = fileContent.replace("\n", "")
 pizzaContent = fileContent[7:]
 pizzaContent = np.array(list(pizzaContent))
 pizza_matrix = np.reshape(pizzaContent, (nr_cols, nr_rows))
+
+
+print ('write file')
+out_path = 'input/output.txt'
+fo = open(out_path, "w")
+
+for ind in range(0,np.size(pizza_matrix,0)):
+    fo.write( str(pizza_matrix[ind,:])+ "\n");
+  # Close file
+fo.close()
 
 print("done")
